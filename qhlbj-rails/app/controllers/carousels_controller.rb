0dd2a424b7afd01900caa33ca1877180f8482bef
lisiurday <lisiurday@gmail.com>
2017-09-16 11:27:00 +0800

    update

--- qhlbj-rails/app/controllers/carousels_controller.rb
+++ qhlbj-rails/app/controllers/carousels_controller.rb
@@ -44,7 +44,7 @@ class CarouselsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_carousel
-      @carousel = Carousel.first
+      @carousel = Carousel.first || Carousel.create
     end
 
     # Only allow a trusted parameter "white list" through.
